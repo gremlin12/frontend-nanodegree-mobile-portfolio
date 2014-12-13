b01bba403efac718e3db24f0f272d671a50f87dd
gremlin12 <zomorabbit@gmail.com>
2014-12-13 08:01:55 -0500

    try something else

--- views/js/main.js
+++ views/js/main.js
@@ -498,7 +498,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+
+var latestKnownScrollY = 0;
+
 function updatePositions() {
+  latestKnownScrollY = window.scrollY;	
   frame++;
   window.performance.mark("mark_start_frame");
 
