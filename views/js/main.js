9aacfc992314d557213c5e378020ac4b0e45d170
gremlin12 <zomorabbit@gmail.com>
2014-12-18 11:31:51 -0500

    Place sin calculation outside for loop

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+
+  var phase = Math.sin(document.body.scrollTop / 1250);
+  for (var i = 0, len = items.length; i < len; i++) {
+    //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
