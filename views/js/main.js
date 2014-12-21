66308fb6258dae862aabc27a20844dd4d7dfb4c5
gremlin12 <zomorabbit@gmail.com>
2014-12-21 07:20:56 -0500

    revert

--- views/js/main.js
+++ views/js/main.js
@@ -540,7 +540,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     //elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    elem.basicLeft = (i % cols) * s + 'px';
+    elem.basicLeft = (i % cols) * s
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
