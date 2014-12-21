68018b8fdf01388d1002ed4d0376b3d8ec6829d6
gremlin12 <zomorabbit@gmail.com>
2014-12-21 10:46:09 -0500

    Revert to transform

--- views/js/main.js
+++ views/js/main.js
@@ -511,8 +511,8 @@ function updatePositions() {
 
   for (var i = 0, len = items.length; i < len; i++) {
     var phase = Math.sin(magicNumber + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px'; 
-    //items[i].style.transform = 'translateX(' + (100*phase) + 'px)';
+    //items[i].style.left = items[i].basicLeft + 100 * phase + 'px'; 
+    items[i].style.transform = 'translateX(' + (100*phase) + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -538,9 +538,9 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.src = "images/pizza_min.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    //elem.style.left = (i % cols) * s + 'px';
+    elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    elem.basicLeft = (i % cols) * s
+    //elem.basicLeft = (i % cols) * s
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
