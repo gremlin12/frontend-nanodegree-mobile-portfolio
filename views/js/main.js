2563966e92566949ca12d0bdcb69e65d4ada6d63
gremlin12 <zomorabbit@gmail.com>
2014-12-22 06:25:35 -0500

    resize slider pizza image

--- views/js/main.js
+++ views/js/main.js
@@ -536,9 +536,9 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza_min.png";
-    //elem.style.height = "100px";
-    //elem.style.width = "73.333px";
+    elem.src = "images/pizza_slider.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
     elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     //elem.basicLeft = (i % cols) * s
